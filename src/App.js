8d01208c1a5aecc29ccb492db820714498b5dec3
Joseph Panetta <jlpanetta1681@icloud.com>
2022-01-23 10:38:25 -0500

    working on getting Track component to tender

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import './App.css';
-import Navbar from './Components/Layout/Navbar'
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import  Index  from './Components/Layout/Index'
+import Navbar from './Components/Layout/Navbar';
 
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <>
+          <Navbar />
+          <div className="container">
+            <Routes>
+              <Route exact path="/" component={ Index } />
+            </Routes>
+          </div>
+        </>
+     </Router>
+
+);
 }
+}
+       
 
-export default App;
+export default App;                 
